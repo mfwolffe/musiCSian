8f96babb4aaf9716c6be533aa1ce7eafd2401b18
Matt Wolffe <wolffemf@dukes.jmu.edu>
2024-12-31 07:07:07 -0500

    global style, plugins

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,13 @@
 import type { Config } from "tailwindcss";
+import flowbite from "flowbite-react/tailwind";
 
+/** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    flowbite.content(),
   ],
   theme: {
     extend: {
@@ -16,6 +19,7 @@ export default {
   },
   plugins: [
     require('daisyui'),
+    flowbite.plugin(),
   ],
 
   daisyui: {
