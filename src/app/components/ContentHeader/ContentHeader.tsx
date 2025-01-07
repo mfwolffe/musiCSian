b5d638fa0351ee9d8158098f90776b17f66cb0b1
mfwolffe <wolffemf@dukes.jmu.edu>
2025-01-06 21:20:09 -0500

    asides for content header comp

--- src/app/components/ContentHeader/ContentHeader.tsx
+++ src/app/components/ContentHeader/ContentHeader.tsx
@@ -1,14 +1,21 @@
+"use client";
+
+import styles from './ContentHeader.module.css'
+
 
 interface ContentHeaderProps {
   title:  string;
+  aside?: string;
   inCard: boolean;
 }
 
 
-const ContentHeader: React.FC<ContentHeaderProps> = ({ title, inCard }) => {
+
+const ContentHeader: React.FC<ContentHeaderProps> = ({ title, inCard, aside='' }) => {
   return (
     <div className="pl-8 pt-6 pb-4">
-      <h1 className={`mr-auto subpixel-antialiased ${inCard ? 'text-2xl' : 'text-4xl'}`}>{ title }</h1>
+      {/* this is atrocious lol */}
+        <h1 className={`mr-auto subpixel-antialiased ${inCard ? 'text-2xl' : 'text-4xl'}`}>{ title } { !aside ? '' : <span className={`text-sm ${styles.asideText}`}>{ aside }</span> }</h1>
     </div>
   );
 }
