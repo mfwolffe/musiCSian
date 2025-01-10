ccd6966ca5dff523721df27055d55d4ed24ece6b
mfwolffe <wolffemf@dukes.jmu.edu>
2025-01-09 22:58:53 -0500

    outreach schema

--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,19 @@
 
 
 interface shortProject {
-  title:          string;
-  imgPath:        string;
-  shortDescript:  string;
-  longerDescript?: string;
+  url?:             string;
+  title:            string;
+  imgPath:          string;
+  shortDescript:    string;
+  longerDescript?:  string;
 }
 
-export type { shortProject }
+interface Outreach {
+  name:             string;
+  role:             string; // not sure if I want to keep this prop
+  date:             string;
+  shortDescript?:   string; // will probably do away w/ for outreach cards
+  longerDescript:   string;
+}
+
+export type { shortProject, Outreach }
