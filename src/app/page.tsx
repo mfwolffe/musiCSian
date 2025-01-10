43f64efca58613e8f9218a0ededf4425dc21accc
mfwolffe <wolffemf@dukes.jmu.edu>
2025-01-10 01:58:38 -0500

    did I forget to commit this?

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
         <AboutMatt />
         <AboutMusic />
         <AboutProjects />
-        <AboutOutreach /> 
+        <AboutOutreach />
       </main>
 
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
