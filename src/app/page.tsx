26632d210c5b28dd076e987308eed87cb2f1bfd5
mfwolffe <wolffemf@dukes.jmu.edu>
2025-01-09 23:01:21 -0500

    scaffold in outreach

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 
 
-import AboutMusic from "./components/Music/Music";
-import AboutMatt  from "./components/MainContent/MainContent";
+import AboutMusic     from "./components/Music/Music";
+import AboutProjects  from "./components/Project/Project";
+import AboutOutreach  from "./components/Outreach/Outreach";
+import AboutMatt      from "./components/MainContent/MainContent";
 import ProfileSidebar from "./components/ProfileSidebar/ProfileSidebar";
-import AboutProjects from "./components/Project/Project";
+
 
 const imgPath = "/images/matt_atlanta2.png";
 const nameHeaderText = "Matthew Forrester Wolffe";
@@ -21,7 +23,8 @@ export default function Home() {
       <main className="flex flex-col gap-8 col-start-2">
         <AboutMatt />
         <AboutMusic />
-        <AboutProjects />  
+        <AboutProjects />
+        <AboutOutreach /> 
       </main>
 
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
