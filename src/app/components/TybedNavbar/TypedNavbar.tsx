b046c7f5c1fa0d2e388303f036b03333edb57c4c
mfwolffe <wolffemf@dukes.jmu.edu>
2025-06-18 21:49:26 -0400

    remove dropdown

--- src/app/components/TybedNavbar/TypedNavbar.tsx
+++ src/app/components/TybedNavbar/TypedNavbar.tsx
@@ -26,31 +26,6 @@ const ProjectsDropDown = () => {
           {/* <summary>Projects</summary> */}
 
           <div tabIndex={0} role="button" className=""><a href="#projectsCard">Projects</a></div>
-          <ul tabIndex={0} className={`dropdown-content menu bg-base-100 rounded-box z-[1] w-96 p-2 shadow ${styles.drop}`}>
-            <ul>
-              <SectionLabel description="Personal" classes="subpixel-antialiased" />
-              <li><a className={`${styles.dropItem}`} href="#">MusicCPR (DAW - Thesis)</a></li>
-              <li><a className={`${styles.dropItem}`} href="#">Terse</a></li>
-              <li><a className={`${styles.dropItem}`} href="#">Primality Instrumentation w/ Intel PIN</a></li>
-              <li><a className={`${styles.dropItem}`} href="#">Digital Dipole</a></li>
-              <li><a className={`${styles.dropItem} font-mono`} href="#">Woof.sh</a></li>
-              <li><a className={`${styles.dropItem} font-mono`} href="#">shellcut</a></li>
-              <li><a className={`${styles.dropItem}`} href="#">A Better aTan2</a></li>
-              <li><a className={`${styles.dropItem} font-mono`} href="#">{"Local{Toast,Roast,Ghost}"}</a></li>
-              <li><a className={`${styles.dropItem}`} href="#">MusicCPR (algorithms for NEA Create)</a></li>
-            </ul>
-            <ul>
-              <SectionLabel description="Selected School" classes="subpixel-antialiased" />
-              <li><a className={`${styles.dropItem}`} href="3">Emulator for a y86 ISA CPU</a></li>
-              <li><a className={`${styles.dropItem}`} href="3">Raccoon Rush</a></li>
-              <li><a className={`${styles.dropItem}`} href="3">dukesh - a shell</a></li>
-            </ul>
-            <ul>
-              <SectionLabel description="Selected Other" classes="subpixel-antialiased" />
-              <li><a className={`${styles.dropItem}`} href="3">'Containerization' w/ Ad hoc Nix scripts</a></li>
-              <li><a className={`${styles.dropItem}`} href="3">'Containerization' w/ custom Arch Linux images</a></li>
-            </ul>
-          </ul>
         {/* </details> */}
       </div>
   )
@@ -70,6 +45,7 @@ const TypedNavbar: React.FC = () => {
       <div className="navbar-end hidden lg:flex">
         <span className='text-xs text-primary self-center'>Jump to:</span>
         <ul className="menu menu-horizontal px-1">
+          {/* TODO: cruft */}
           <li>
             <ProjectsDropDown />
           </li>
